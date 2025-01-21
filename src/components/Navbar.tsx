1afb859d5fad85bddaadab6c0751c11667c38879
tagdabanda69 <tagdabusinessadda@gmail.com>
2025-01-21 20:43:30 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../stores/authStore';
-import logo from '../assets/logo-png.png'; // Import the logo image
+import logo from '../assets/logo-transparent-png (1) (1)'; // Import the logo image
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
